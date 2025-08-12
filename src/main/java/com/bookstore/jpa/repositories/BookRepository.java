5425ceb006195d08e95df6ef64c0c2d9fac06a93
pedro-mileipp <poncianopedro70@gmail.com>
2025-08-12 13:57:51 -0300

    Adiciona camada de controllers e conserta metodo findBookModelByTtile em BookRepository, tambem adiciona os dtos

--- src/main/java/com/bookstore/jpa/repositories/BookRepository.java
+++ src/main/java/com/bookstore/jpa/repositories/BookRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.repository.query.Param;
 
 import com.bookstore.jpa.models.BookModel;
 
-public interface BookRepositorie extends JpaRepository<BookModel, UUID> {
-    BookModel finBookModelByTitle(String title);
+public interface BookRepository extends JpaRepository<BookModel, UUID> {
+    BookModel findBookModelByTitle(String title);
 
     @Query(value = "SELECT * FROM tb_book WHERE publisher_id = := id", nativeQuery = true)
     List<BookModel> findBooksByPublisherId(@Param("id") UUID id);
